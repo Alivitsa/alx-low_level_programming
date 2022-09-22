1b4087259eb629c2c588b9bebda9459672996bb2
Alivitsa <lorrainealivitsa@gmail.com>
2022-09-22 12:44:42 -0700

    pos neg2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 
 /**
  * main - Prints if number is positive, zero or negative
@@ -12,7 +11,7 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() RAND_MAX/2;
+	n = rand() RAND_MAX / 2;
 
 	if(n>0)
 	{
